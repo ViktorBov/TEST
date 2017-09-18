38fa0482205e9ebe6cb0530580c3ce510dd4cdb6
Viktor <viktor.bovsdarenko@centralinnovation.com>
2017-09-18 15:12:06 +1000

    PWB_1

--- CommonProject/Class1.cs
+++ CommonProject/Class1.cs
@@ -10,5 +10,9 @@ namespace CommonProject
         //Common Project
         //Another common change in dev branch
         //Will Commit And Create another branch 3:06 PM 18/09/2017
+
+        //Commited to dev branch 3:10 PM 18/09/2017
+        //Created PWB branch 3:10 PM 18/09/2017
+        //Commited to PWB branch 3:11 PM 18/09/2017
     }
 }
