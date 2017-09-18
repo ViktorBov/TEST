1a47ee367acb93b8afa4ab9e0e04bff58ca10756
Viktor <viktor.bovsdarenko@centralinnovation.com>
2017-09-18 15:25:03 +1000

    Nothing below dotted line should get into DEV branch

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -12,8 +12,12 @@ namespace GitTest
             Console.WriteLine("Changed project in TEST_2");
             Console.WriteLine("Working in DEV branch");
             Console.WriteLine("Changed project in TEST_2 3:00 PM");
+
+            //-----------------------------------------------------
             //Making Changes to PDW branch 3:18 PM 18/09/2017
             //Making Changes to PDW branch in TEST_2 3:20 PM 18/09/2017
+            //Nothing below dotted line should get into DEV branch
+
         }
     }
 }
